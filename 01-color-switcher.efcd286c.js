c99a599868cb5c247a0d8438860390be1080d9e7
ElizaChernysheva <91914018+ElizaChernysheva@users.noreply.github.com>
2022-05-29 18:38:12 +0000

    Deploying to gh-pages from @ ElizaChernysheva/goit-js-hw-09@28578fe654ae30492e6491f996ba83c3b154d9f5 🚀

---
+++ 01-color-switcher.efcd286c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(t=>{t.currentTarget.setAttribute("disabled","disabled"),e.removeAttribute("disabled"),timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));e.addEventListener("click",(e=>{e.currentTarget.setAttribute("disabled","disabled"),t.removeAttribute("disabled"),clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.efcd286c.js.map
